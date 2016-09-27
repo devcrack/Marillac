9ec5744ae5e660664f5c3a090ec79a80b4899b45
Cesar Josue <cesar_torres8@hotmail.com>
2016-09-27 16:36:08 -0500

    Revert "Avance en Actividad"
    This reverts commit 7c47fd47035199439c83689b2b076d51c70c24f8.

--- WindowsFormsApplication1/WindowsFormsApplication1/Form_Actividades.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form_Actividades.cs
@@ -15,7 +15,6 @@ namespace WindowsFormsApplication1
     {
         Menu main_Form;
 
-        private Int64 idAct;
         private SqlConnection con;
         private SqlDataAdapter adapter;
         private string connectionString;
@@ -24,7 +23,6 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
             this.main_Form = m_Form;
-            idAct = 0;
 
             con = new SqlConnection();
             connectionString = @"Data Source=MAUVALDES\SQLEXPRESS;Initial Catalog=Marillac;Integrated Security=True";
@@ -40,10 +38,14 @@ namespace WindowsFormsApplication1
 
         private void Form_Actividades_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'marillacDataSet.InscripcionHijo' table. You can move, or remove it, as needed.
+            this.inscripcionHijoTableAdapter.Fill(this.marillacDataSet.InscripcionHijo);
+
+            // TODO: This line of code loads data into the 'marillacDataSet.InscripcionBeneficiario' table. You can move, or remove it, as needed.
+            this.inscripcionBeneficiarioTableAdapter.Fill(this.marillacDataSet.InscripcionBeneficiario);
+
+            // TODO: This line of code loads data into the 'marillacDataSet.Actividad' table. You can move, or remove it, as needed.
             this.actividadTableAdapter.Fill(this.marillacDataSet.Actividad);
-            this.beneficiarioTableAdapter.FillBy(this.marillacDataSet.Beneficiario, idAct);
-            dataGridView1.ClearSelection();
-            limpiarTextBox();
         }
 
         //BOTÓN PARA SALIR
@@ -143,6 +145,16 @@ namespace WindowsFormsApplication1
             }
         }
 
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void idProfesor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
         //FUNCIÓN QUE LLENA LOS TEXT BOX CUANDO SE SELECCIONA UN CAMPO DEL DATA GRID
         private void dataGridView1_SelectionChanged_1(object sender, EventArgs e)
         {
@@ -155,25 +167,11 @@ namespace WindowsFormsApplication1
                 cupo.Text = Convert.ToString(dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[6].Value);
                 costo.Text = Convert.ToString(dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[7].Value);
                 idProfesor.Text = Convert.ToString(dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[1].Value);
-                idAct = Convert.ToInt64(dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[0].Value.ToString());
-                this.beneficiarioTableAdapter.FillBy(this.marillacDataSet.Beneficiario, idAct);
             }
             catch
             {
             }
         }
 
-        private void fillByToolStripButton_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                this.beneficiarioTableAdapter.FillBy(this.marillacDataSet.Beneficiario, ((long)(System.Convert.ChangeType(claveActividadToolStripTextBox.Text, typeof(long)))));
-            }
-            catch (System.Exception ex)
-            {
-                System.Windows.Forms.MessageBox.Show(ex.Message);
-            }
-
-        }
     }
 }
