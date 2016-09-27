fa02c367e8f7caa04923f340ec4dff4f9cf20757
mvaldesb <mau_v_b20@hotmail.com>
2016-09-27 17:03:19 -0500

    Revert "Mauricio Commint"
    This reverts commit c4d23b84a1bd033e852dbc18a2ff12750ae20157.

--- WindowsFormsApplication1/WindowsFormsApplication1/beneficiarioActividades.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/beneficiarioActividades.Designer.cs
@@ -29,196 +29,28 @@
         private void InitializeComponent()
         {
             this.label9 = new System.Windows.Forms.Label();
-            this.idProfesor = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.costo = new System.Windows.Forms.TextBox();
-            this.horaFinal = new System.Windows.Forms.TextBox();
-            this.horaInicio = new System.Windows.Forms.TextBox();
-            this.diasImpartida = new System.Windows.Forms.TextBox();
-            this.nombreActividad = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Calibri", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(250, 9);
+            this.label9.Location = new System.Drawing.Point(250, 22);
             this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(278, 66);
             this.label9.TabIndex = 11;
             this.label9.Text = "Actividades";
             // 
-            // idProfesor
-            // 
-            this.idProfesor.Font = new System.Drawing.Font("Calibri", 14F);
-            this.idProfesor.Location = new System.Drawing.Point(114, 171);
-            this.idProfesor.Margin = new System.Windows.Forms.Padding(2);
-            this.idProfesor.Name = "idProfesor";
-            this.idProfesor.Size = new System.Drawing.Size(266, 30);
-            this.idProfesor.TabIndex = 48;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Calibri", 14F);
-            this.label8.Location = new System.Drawing.Point(11, 178);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(86, 23);
-            this.label8.TabIndex = 49;
-            this.label8.Text = "Profesor: ";
-            // 
-            // costo
-            // 
-            this.costo.Font = new System.Drawing.Font("Calibri", 14F);
-            this.costo.Location = new System.Drawing.Point(114, 239);
-            this.costo.Margin = new System.Windows.Forms.Padding(2);
-            this.costo.Name = "costo";
-            this.costo.Size = new System.Drawing.Size(87, 30);
-            this.costo.TabIndex = 47;
-            // 
-            // horaFinal
-            // 
-            this.horaFinal.Font = new System.Drawing.Font("Calibri", 14F);
-            this.horaFinal.Location = new System.Drawing.Point(283, 205);
-            this.horaFinal.Margin = new System.Windows.Forms.Padding(2);
-            this.horaFinal.Name = "horaFinal";
-            this.horaFinal.Size = new System.Drawing.Size(97, 30);
-            this.horaFinal.TabIndex = 46;
-            // 
-            // horaInicio
-            // 
-            this.horaInicio.Font = new System.Drawing.Font("Calibri", 14F);
-            this.horaInicio.Location = new System.Drawing.Point(161, 205);
-            this.horaInicio.Margin = new System.Windows.Forms.Padding(2);
-            this.horaInicio.Name = "horaInicio";
-            this.horaInicio.Size = new System.Drawing.Size(97, 30);
-            this.horaInicio.TabIndex = 45;
-            // 
-            // diasImpartida
-            // 
-            this.diasImpartida.Font = new System.Drawing.Font("Calibri", 14F);
-            this.diasImpartida.Location = new System.Drawing.Point(114, 134);
-            this.diasImpartida.Margin = new System.Windows.Forms.Padding(2);
-            this.diasImpartida.Name = "diasImpartida";
-            this.diasImpartida.Size = new System.Drawing.Size(266, 30);
-            this.diasImpartida.TabIndex = 44;
-            // 
-            // nombreActividad
-            // 
-            this.nombreActividad.Font = new System.Drawing.Font("Calibri", 14F);
-            this.nombreActividad.Location = new System.Drawing.Point(114, 100);
-            this.nombreActividad.Margin = new System.Windows.Forms.Padding(2);
-            this.nombreActividad.Name = "nombreActividad";
-            this.nombreActividad.Size = new System.Drawing.Size(266, 30);
-            this.nombreActividad.TabIndex = 43;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Calibri", 14F);
-            this.label6.Location = new System.Drawing.Point(11, 246);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(63, 23);
-            this.label6.TabIndex = 42;
-            this.label6.Text = "Costo: ";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Calibri", 14F);
-            this.label3.Location = new System.Drawing.Point(11, 212);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(146, 23);
-            this.label3.TabIndex = 40;
-            this.label3.Text = "Hora Inicio/Final: ";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Calibri", 14F);
-            this.label2.Location = new System.Drawing.Point(11, 141);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 23);
-            this.label2.TabIndex = 39;
-            this.label2.Text = "Días: ";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Calibri", 14F);
-            this.label1.Location = new System.Drawing.Point(11, 107);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 23);
-            this.label1.TabIndex = 38;
-            this.label1.Text = "Nombre: ";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Calibri", 14F);
-            this.label5.Location = new System.Drawing.Point(263, 208);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(16, 23);
-            this.label5.TabIndex = 50;
-            this.label5.Text = "-";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Calibri", 14F);
-            this.textBox1.Location = new System.Drawing.Point(293, 242);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(87, 30);
-            this.textBox1.TabIndex = 51;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Calibri", 14F);
-            this.label4.Location = new System.Drawing.Point(228, 249);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 23);
-            this.label4.TabIndex = 52;
-            this.label4.Text = "Saldo: ";
-            // 
             // beneficiarioActividades
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(820, 464);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.idProfesor);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.costo);
-            this.Controls.Add(this.horaFinal);
-            this.Controls.Add(this.horaInicio);
-            this.Controls.Add(this.diasImpartida);
-            this.Controls.Add(this.nombreActividad);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(820, 364);
             this.Controls.Add(this.label9);
             this.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.MaximizeBox = false;
             this.Name = "beneficiarioActividades";
             this.ShowIcon = false;
@@ -231,19 +63,5 @@
         #endregion
 
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox idProfesor;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox costo;
-        private System.Windows.Forms.TextBox horaFinal;
-        private System.Windows.Forms.TextBox horaInicio;
-        private System.Windows.Forms.TextBox diasImpartida;
-        private System.Windows.Forms.TextBox nombreActividad;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label4;
     }
 }
