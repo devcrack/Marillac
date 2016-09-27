f7b9f8c6316632c031ac7ee01da29a4ccae3473f
mvaldesb <mau_v_b20@hotmail.com>
2016-09-27 17:04:05 -0500

    Revert "Revert "Mauricio Commint""
    This reverts commit fa02c367e8f7caa04923f340ec4dff4f9cf20757.

--- WindowsFormsApplication1/WindowsFormsApplication1/F_Beneficiario.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/F_Beneficiario.cs
@@ -17,6 +17,7 @@ namespace WindowsFormsApplication1
         Form beneficiarioPsicologo;
         Form hijosDeBeneficiario;
         Form actividadBeneficiario;
+        Form pagosBeneficiario;
 
         private SqlConnection con;
         private SqlDataAdapter adapter;
@@ -251,6 +252,18 @@ namespace WindowsFormsApplication1
                 catch { idBen = -1; }
                 this.actividadBeneficiario = new beneficiarioActividades(this, connectionString, idBen);
                 this.actividadBeneficiario.ShowDialog();
+            }
+        }
+
+        private void botonPagos_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count != 0)
+            {
+                Int64 idBen;
+                try { idBen = Convert.ToInt64(dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[0].Value.ToString()); }
+                catch { idBen = -1; }
+                this.pagosBeneficiario = new cuotaBeneficiario(this, connectionString, idBen);
+                this.pagosBeneficiario.ShowDialog();
             }
         }
     }
