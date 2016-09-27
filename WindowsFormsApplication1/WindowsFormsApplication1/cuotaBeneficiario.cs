c4d23b84a1bd033e852dbc18a2ff12750ae20157
mvaldesb <mau_v_b20@hotmail.com>
2016-09-27 17:00:06 -0500

    Mauricio Commint

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/cuotaBeneficiario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public partial class cuotaBeneficiario : Form
+    {
+        private Form main_form;
+        private Int64 idBen;
+
+        private SqlConnection con;
+        private SqlDataAdapter adapter;
+        private string connectionString;
+
+        public cuotaBeneficiario(Form m_form, string c, Int64 id)
+        {
+            InitializeComponent();
+            this.main_form = m_form;
+
+            con = new SqlConnection();
+            connectionString = c;
+            con.ConnectionString = connectionString;
+            adapter = new SqlDataAdapter();
+        }
+    }
+}
